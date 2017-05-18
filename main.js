f4cd27a38bcb9106085c91d2d1fd95522ba7f1eb
nexisn4 <yckim@ucdavis.edu>
2017-05-17 22:36:49 -0700

    upload dropdown added upload file selector added upload file added

--- main.js
+++ main.js
@@ -1,5 +1,47 @@
+var port = 8650;
+
+function toggleUpload() {
+      document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 
-upload(){
-  
+function uploadFile(){
+  // uploads an image within a form object.  This currently seems
+// to be the easiest way to send a big binary file.
+    var url = "http://138.68.25.50:" + port;
+
+    // where we find the file handle
+    var selectedFile = document.getElementById('fileSelector').files[0];
+    var formData = new FormData();
+    // stick the file into the form
+    formData.append("userfile", selectedFile);
+
+    // more or less a standard http request
+    var oReq = new XMLHttpRequest();
+    // POST requests contain data in the body
+    // the "true" is the default for the third param, so
+    // it is often omitted; it means do the upload
+    // asynchornously, that is, using a callback instead
+    // of blocking until the operation is completed.
+    oReq.open("POST", url, true);
+    oReq.onload = function() {
+		// the response, in case we want to look at it
+		console.log(oReq.responseText);
+    }
+    oReq.send(formData);
 }
